70b563c5ff188e9eec3067456be7d537cc96d3d7
Rociozvl <rociogalbanshinox@gmail.com>
2022-08-18 13:50:56 -0300

    actualizado

--- src/index.js
+++ src/index.js
@@ -21,5 +21,5 @@ app.use(indexRoutes)
 
 // app.get('/contact',(req , res)=>  res.render('contact'))
 app.use(express.static(join(__dirname,'public')))
-app.listen(3000)
-console.log('server is listening on port', 3000)
+app.listen(process.env.PORT || 3000)
+console.log('server is listening on port', process.env.PORT || 3000)
